71927bb86c7ce3f4d608bbcbc5744e4a23020b8b
taeraemon <dbsl0512@naver.com>
2025-05-04 20:35:05 +0900

    Update PT viewer

--- handler/label/handler_label_group.py
+++ handler/label/handler_label_group.py
@@ -10,6 +10,18 @@ class HandlerLabelGroup:
             "gps_lon": HandlerLabel(ui.LB_GPS_LON, "{:.6f}"),
             "gps_alt": HandlerLabel(ui.LB_GPS_ALT, "{:.2f} m"),
             "gps_sat": HandlerLabel(ui.LB_GPS_SAT, "{} sats"),
+            "LB_PNID_PT_1": HandlerLabel(ui.LB_PNID_PT_1, "{:.2f}"),
+            "LB_PNID_PT_2": HandlerLabel(ui.LB_PNID_PT_2, "{:.2f}"),
+            "LB_PNID_PT_3": HandlerLabel(ui.LB_PNID_PT_3, "{:.2f}"),
+            "LB_PNID_PT_4": HandlerLabel(ui.LB_PNID_PT_4, "{:.2f}"),
+            "LB_PNID_PT_5": HandlerLabel(ui.LB_PNID_PT_5, "{:.2f}"),
+            "LB_PNID_PT_6": HandlerLabel(ui.LB_PNID_PT_6, "{:.2f}"),
+            "LB_PNID_PT_7": HandlerLabel(ui.LB_PNID_PT_7, "{:.2f}"),
+            "LB_PNID_PT_8": HandlerLabel(ui.LB_PNID_PT_8, "{:.2f}"),
+            "LB_PNID_PT_9": HandlerLabel(ui.LB_PNID_PT_9, "{:.2f}"),
+            "LB_PNID_PT_10": HandlerLabel(ui.LB_PNID_PT_10, "{:.2f}"),
+            "LB_PNID_PT_11": HandlerLabel(ui.LB_PNID_PT_11, "{:.2f}"),
+            "LB_PNID_PT_12": HandlerLabel(ui.LB_PNID_PT_12, "{:.2f}"),
         }
 
     def update_all(self, data: DataVehicle):
@@ -17,3 +29,9 @@ class HandlerLabelGroup:
         self.handlers["gps_lon"].update(data.gps_lon)
         self.handlers["gps_alt"].update(data.gps_alt)
         self.handlers["gps_sat"].update(data.gps_sat)
+
+        # Update QLabel values based on data.pt
+        for i, value in enumerate(data.pt):
+            label_name = f"LB_PNID_PT_{i+1}"
+            if label_name in self.handlers:
+                self.handlers[label_name].update(value)
